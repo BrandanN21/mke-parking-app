1606205fc6a6de08c3169fca497c6a53b54209d2
BrandanN21 <47616844+BrandanN21@users.noreply.github.com>
2021-03-08 16:48:11 -0600

    setup homepage and started navigation

--- App.js
+++ App.js
@@ -1,28 +1,49 @@
+
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Touchable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MapScreen } from './src/screens/mapscreen/MapScreen';
+import { HomeScreen } from './src/screens/homescreen/HomeScreen';
 
-export default function App() {
+
+const Tab = createBottomTabNavigator();
+
+ export default function App() {
   return (
-    <View>
-      {/* <ImageBackground source={require('./assets/loading_page.png')}
-      style={styles.background}
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home';
+            } else if (route.name === 'Map') {
+              iconName = focused ? 'map-marked-alt' : 'map-marker';
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+            
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'orange',
+          inactiveTintColor: 'gray',
+        }}
       >
-      </ImageBackground> */}
-      <Text>Hello Welcome to the app</Text>
-    </View>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Map" component={MapScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  background: {
-    width: '100%',
-    height: '100%'
-  }
-});
+
+
+
+
--- src/screens/mapscreen/MapScreen.js
+++ src/screens/mapscreen/MapScreen.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export const MapScreen = () => {
     return (
-        <div>
-            
-        </div>
+        <View>
+            <Text style={styles.text}>Map page</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        padding: "2%",
+        fontSize:  33,
+        top: '50%'
+        
+
+    }
+
+})
 
 export default MapScreen;
