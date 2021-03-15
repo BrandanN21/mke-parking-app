5846b98876dfe4b206c5b8958b9db47fd3ea3dc0
BrandanN21 <47616844+BrandanN21@users.noreply.github.com>
2021-03-14 20:26:04 -0500

    not working-but added permitscreen

--- src/screens/permitscreen/PermitScreen.js
+++ src/screens/permitscreen/PermitScreen.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export function PermitScreen() {
     return (
-        <View>
-            <Text style={styles.text}>This is the permits screen</Text>
+        <View><Text style={styles.text}>Permits Screen</Text></View>
+        // need three buttons rounded
+        // one for night parking linking to the city of mke webiste
+        // one for daytime
+        // one for snow emergency
+        // <NavigationContainer>
+        //     <Stack.Navigator initialRouteName="Permit">
+        //         <Stack.Screen name="Permit" component={PermitScreen} />
+        //         <Stack.Screen name="NightParking" component={NightParking} />
+        //     </Stack.Navigator>
+        // </NavigationContainer>
+    )
+}
+
+function NightParking(){
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate('Details')}
+            />
         </View>
     )
 }
