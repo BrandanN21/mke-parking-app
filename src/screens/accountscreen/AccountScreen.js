fbc4019949240e475dcc41028690a0221bd50bc1
BrandanN21 <47616844+BrandanN21@users.noreply.github.com>
2021-03-29 11:04:19 -0500

    started building account screen
    added image and styling for account image

--- src/screens/accountscreen/AccountScreen.js
+++ src/screens/accountscreen/AccountScreen.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableHighlight, Image} from 'react-native'
 
 export default function AccountScreen() {
     return (
         <View>
-            <Text style={styles.text}>Account Page</Text>
+            <TouchableHighlight
+            style={[styles.profileImgContainer, { borderColor: 'green', borderWidth:1 }]}
+            >
+                <Image source={require("../../../assets/persona.jpg")}
+                style={styles.profileImg} />
+            </TouchableHighlight>
+            <Text style={styles.text}>Katlyne Featherstone</Text>
         </View>
     )
 }
+
 
 // styles for account page
 const styles = StyleSheet.create({
@@ -23,8 +30,19 @@ const styles = StyleSheet.create({
         padding: "2%",
         fontSize:  33,
         top: '50%'
-        
-
-    }
+    },
+    profileImgContainer: {
+        marginLeft: '35%',
+        top: '40%',
+        height: 160,
+        width: 160,
+        borderRadius: 80,
+      },
+      profileImg: {
+        resizeMode: "cover",
+        height: 160,
+        width: 160,
+        borderRadius: 80,
+      },
 
 })
