ffa547515c8480534ec32b871fd6fe9f73d1a0a9
BrandanN21 <47616844+BrandanN21@users.noreply.github.com>
2021-04-04 23:08:01 -0500

    added settings/account screen
    added package to create basic settings and account screen

--- src/screens/accountscreen/AccountScreen.js
+++ src/screens/accountscreen/AccountScreen.js
@@ -1,16 +1,67 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableHighlight, Image} from 'react-native'
+import { View, Text, StyleSheet, TouchableHighlight, Image} from 'react-native';
+import { SectionRow, SettingsPage, NavigateRow, BaseRow, CheckRow, SwitchRow } from 'react-native-settings-view';
+
+
 
 export default function AccountScreen() {
     return (
-        <View>
-            <TouchableHighlight
-            style={[styles.profileImgContainer, { borderColor: 'green', borderWidth:1 }]}
-            >
-                <Image source={require("../../../assets/persona.jpg")}
-                style={styles.profileImg} />
-            </TouchableHighlight>
-            <Text style={styles.text}>Katlyne Featherstone</Text>
+        <View style={styles.container}>
+            <View>
+                <TouchableHighlight
+                style={[styles.profileImgContainer, { borderColor: 'green', borderWidth:1 }]}
+                >
+                    <Image source={require("../../../assets/persona.jpg")}
+                    style={styles.profileImg} />
+                </TouchableHighlight>
+                <Text style={styles.text}>Eliana Featherstone</Text>
+            </View>
+
+            <SettingsPage style={styles.settings}>
+                <SectionRow>
+                    <NavigateRow
+                    text="Terms and conditions"
+                    leftIcon={{
+                        name: 'file-document',
+                        type: 'material-community',
+                    }}
+                    onPress={() => console.log('terms')}
+                    />
+                    <NavigateRow
+                    text="Privacy Policy"
+                    leftIcon={{
+                        name: 'folder-lock',
+                        type: 'material-community',
+                    }}
+                    onPress={() => console.log('policy')}
+                    />
+                    <NavigateRow
+                    text="Contact us"
+                    leftIcon={{
+                        name: 'users',
+                        type: 'font-awesome',
+                    }}
+                    onPress={() => console.log('contact')}
+                    />
+                    <SwitchRow
+                    text="Notifications"
+                    enabled
+                    leftIcon={{
+                        name: 'ios-notifications',
+                        type: 'ionicon',
+                    }}
+                    onValueChange={(isEnabled) => console.log('checked', isEnabled)}
+                    />
+                    <BaseRow
+                    text={('version')}
+                    leftIcon={{
+                        name: 'tag',
+                        type: 'font-awesome',
+                    }}
+                    rightContent={<Text>0.0.1</Text>}
+                    />
+                </SectionRow>
+            </SettingsPage>
         </View>
     )
 }
@@ -21,19 +72,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        top: '0%'
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
     text: {
         textAlign: 'center',
         fontWeight: 'bold',
         padding: "2%",
         fontSize:  33,
-        top: '50%'
+        top: '30%'
+    },
+    profileContainer: {
+        height: '100%',
+        marginBottom: 15
     },
     profileImgContainer: {
-        marginLeft: '35%',
-        top: '40%',
+        marginLeft: '33%',
+        top: '25%',
         height: 160,
         width: 160,
         borderRadius: 80,
@@ -44,5 +100,8 @@ const styles = StyleSheet.create({
         width: 160,
         borderRadius: 80,
       },
+      settings: {
+          top: '10%'
+      }
 
 })
